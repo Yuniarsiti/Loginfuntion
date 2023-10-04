21e3a354b5de15ba968c75da7f57e8fd33a40a65
Yuniarsiti <141316968+Yuniarsiti@users.noreply.github.com>
2023-10-04 14:57:50 +0700

    Add files via upload

---
+++ regulanfrun.js
@@ -0,0 +1,9 @@
+function login (username,pass) {
+    console.log("...............")
+    console.log("login")
+    console.log(".........")
+    console.log("ussername:"+username)
+    console.log("pasword:"+pass)
+}
+
+login("yuniar","************")
